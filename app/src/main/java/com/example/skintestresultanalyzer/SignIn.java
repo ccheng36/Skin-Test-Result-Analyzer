0eed18c94aff02af65985eec3b5372463a3031a8
Danny Cao <zoreflyc@gmail.com>
2019-03-22 20:58:40 -0700

    login

---
+++ app/src/main/java/com/example/skintestresultanalyzer/SignIn.java
@@ -0,0 +1,72 @@
+package com.example.skintestresultanalyzer;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.skintestresultanalyzer.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class SignIn extends AppCompatActivity {
+
+    EditText email, password;
+    Button SignInBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        email = (MaterialEditText) findViewById(R.id.email);
+        password = (MaterialEditText) findViewById(R.id.password);
+        SignInBtn = (Button) findViewById(R.id.SignUpBtn);
+
+        // initialize firebase
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_ucsfstra = database.getReference("ucsf-stra");
+
+        SignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ProgressDialog mDialog = new ProgressDialog(SignIn.this);
+                mDialog.setMessage("Please wait...");
+                mDialog.show();
+
+                table_ucsfstra.addValueEventListener(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(email.getText().toString()).exists()) {
+                            mDialog.dismiss();
+
+                            User user = dataSnapshot.child(email.getText().toString()).getValue(User.class);
+
+                            if (user.getPassword().equals(password.getText().toString())) {
+                                Toast.makeText(SignIn.this, "Sign in successfully!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(SignIn.this, "Sign in fail", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(SignIn.this, "User not exist in database", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+}
