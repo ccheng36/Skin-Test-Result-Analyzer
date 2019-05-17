36acb14d03e066a08568cff1d34c3cd854dab143
ZoREfLy <scao22@gmail.com>
2019-05-16 22:00:47 -0700

    update

--- openCVLibrary346/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary346/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\scao22\\Desktop\\SkinTestResultAnalyzer\\openCVLibrary346\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: D:\\Users\\Shund\\AndroidStudioProjects\\SkinTestResultAnalyzer\\openCVLibrary346\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
